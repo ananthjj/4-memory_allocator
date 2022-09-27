4dad1c9bbd5ffd8f099e49d2ee3afa6832d60a6c
ananth <ananth.josyula@gmail.com>
2022-09-26 23:39:13 -0500

    Final Submission v2

--- memory.c
+++ memory.c
@@ -28,6 +28,14 @@ void memory_init(void* buffer, size_t size)
 
 size_t memory_avail()
 {
+  size_t count = 0;
+  for (size_t i = 0; i<num_frames; i++){
+    if(info_ptr[i] == '0')
+      count += FRAME_SIZE;
+  }
+  return count;
+
+  /*
   for(size_t i = 0; i<num_frames; i++){
        printf("*** %c",info_ptr[i]);
     }
@@ -36,20 +44,28 @@ size_t memory_avail()
     if (info_ptr[i] == '0')
       count++;
   }
-  return count * FRAME_SIZE;
+  return count * FRAME_SIZE;*/
 }
 
 void* memory_alloc(size_t size)
 {
   if (memory_avail() < size)
     return NULL;
-  size_t frames  = round_up(size/FRAME_SIZE);
+  size_t frames  = (size + FRAME_SIZE - 1) / FRAME_SIZE;
   for (size_t i = 0; i<num_frames; i++){
     if(info_ptr[i] == '0'){
-      for (size_t j = 0; j<frames; j++){
-	info_ptr[j] = '1';
+      //size_t count = 1;
+      /*for (size_t k = 1; k < frames; k++){
+	if (info_ptr[i+k] == 0)
+	  count++;
+	else
+	  break;
       }
-      return data_ptr + i * FRAME_SIZE;
+      if (count == frames){*/
+	info_ptr[i] = '1';
+	for (size_t j = 1; j<frames; j++)
+	  info_ptr[i+j] = '2';
+	return data_ptr + i * FRAME_SIZE;
     }
   }
   return NULL;
@@ -58,11 +74,41 @@ void* memory_alloc(size_t size)
 
 void memory_free(void* ptr)
 {
-  for (size_t i = (ptr-data_ptr)/FRAME_SIZE; i<num_frames; i++){
+  size_t i = (ptr-data_ptr)/FRAME_SIZE;
+  if (i<num_frames && info_ptr[i] == '1' && i>=0){
     info_ptr[i]='0';
+    for (size_t j = i+1; j  < num_frames && info_ptr[j] == '2'; j++){
+      info_ptr[i+j] = '0';
+    }
   }
 }
 
 void test3()
 {
+  printf("TEST 3 STARTED\n");
+  
+  size_t LENGTH = FRAME_SIZE * 2;
+  
+  char* ptr1 = memory_alloc(LENGTH);
+  printf("allocated %ld memory at %p\n", LENGTH, ptr1);
+
+  char* ptr2 = memory_alloc(LENGTH);
+  printf("allocated %ld memory at %p\n", LENGTH, ptr2);
+  
+  char* ptr3 = memory_alloc(LENGTH);
+  printf("allocated %ld memory at %p\n", LENGTH, ptr3);
+
+  size_t len1 = memory_avail();
+  printf("available memory after alloc is %ld\n", len1);
+
+  memory_free(ptr2);
+  printf("memory is freed at %p\n", ptr2);
+
+  size_t len2 = memory_avail();
+  printf("available memory after free is %ld\n", len2);
+
+  if (len2 == 3*LENGTH)
+    printf("TEST 3 PASSED\n");
+  else
+    printf("TEST 3 FAILED\n");
 }
